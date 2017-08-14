80296624de4cc6920dc6e67fa7be13d67d509032
Dane Miller <dane@Danes-MacBook-Pro.local>
2017-08-14 00:28:21 -0500

    Setup basic layout and created outlets

---
+++ MemeMe/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  MemeMe
+//
+//  Created by Dane Miller on 8/14/17.
+//  Copyright © 2017 Dane Miller. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var topTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func takeMemePhoto(_ sender: Any) {
+    }
+    
+    @IBAction func pickMemeImage(_ sender: Any) {
+    }
+    
+
+    
+
+}
